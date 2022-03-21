51b86c365dbc029461098f4f6c37d135b9358647
marekmitko <mitko.email@gmail.com>
2022-03-21 15:42:18 +0100

    checkpoint(empty): commit record - preRefactoring BuyerDataFromLayout

--- invoice-app/src/components/invoice-form/personal-cards/buyer/TestBuyerCard.js
+++ invoice-app/src/components/invoice-form/personal-cards/buyer/TestBuyerCard.js
@@ -84,8 +84,8 @@ const dataBuyer = {};
                         <BoxTextInput label="ZIP Code" source="addres.ZIPCode" resource="buyer" mr="0.5em" />
                         <BoxTextInput label="City Name" flex={2} source="addres.city" resource="buyer"/>
                     </Box>
-                    <BoxTextInput flex={2} mt="0" mb="-1em" source="contact.email" resource="buyer" type="email" />
-                    <Box display="flex" mt="-1em" mb="-1.5em">
+                    {/* <BoxTextInput flex={2} mt="0" mb="-1em" source="contact.email" resource="buyer" type="email" /> */}
+                    <Box display="flex" mt="1em" mb="-1.5em">
                         <BoxTextInput label="MVA Code" mt="-0.75em"  mb="-1em"    source="buyerMVA"  initialValue={dataBuyer.noMVA}    mr="0.5em"  />
                         <BooleanInput label="MVA" 
                             // source={dataBuyer.noMVA} 
