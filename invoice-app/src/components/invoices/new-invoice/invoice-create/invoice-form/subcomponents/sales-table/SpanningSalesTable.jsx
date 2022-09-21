f122d064f7e6c9c8fbab798879bd8451644d6e1a
Marek Mitko <mitko.email@gmail.com>
2022-09-21 15:50:07 +0200

    feat(TableTotalSum): added calc total sum

--- invoice-app/src/components/invoices/new-invoice/invoice-create/invoice-form/subcomponents/sales-table/SpanningSalesTable.jsx
+++ invoice-app/src/components/invoices/new-invoice/invoice-create/invoice-form/subcomponents/sales-table/SpanningSalesTable.jsx
@@ -1,38 +1,20 @@
 import * as React from 'react';
 import { useForm } from "react-hook-form";
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
 import TableHeader from './spanning-sales-table/TableHeader';
-import { TableFooter } from '@mui/material';
+import { Paper, TableContainer, Table, TableCell, TableRow, TableFooter } from '@mui/material';
 import TableTotalSum from './spanning-sales-table/TableTotalSum';
 import SalesTableList from './spanning-sales-table/SalesTableList';
+import { SalesTotalSum } from './spanning-sales-table/total-sum-table/CalcTotalSum';
 
 
 /**DEFAULT VALUES FOR THE SpanningSalesTable */
-
-//default values sales item
-const rows = [
-    createSalesItem( 1, 'Paperclips (Box)', 'Services', 100, 10, 15),
-];
 function setGrosssPriceSalesItem(netPrice, taxValue) {
     return ((netPrice * ((taxValue/100)+1)));
 }
-//auxiliary function create data default value sales item
-//get value output
 function createSalesItem( item_id, desc, type, qty, netPrice, taxValue) {
     const grossPrice = setGrosssPriceSalesItem(netPrice, taxValue);
     return { item_id, desc, type, qty, netPrice, taxValue, grossPrice };
 }
-
-// NEW APPROACH
-
-const TESTdefaultValuesSalesItem = createSalesItem(1, 'Paperclips (Box)', 'Services', 100, 10, 1500);
-// const defaultValues = { item_id: "",  itemName: "",  type: "",  qty: 1, netPrice: "",  taxValue: "",  grossPrice: ""}
 
 const defaultValuesSalesItem = {
     item_id: "",
@@ -50,34 +32,13 @@ const defaultValues = {
             item_id: "",
             itemName: "",
             type: "",
-            qty: 1,
+            qty: "",
             netPrice: "",
             taxValue: "",
             grossPrice: "",
         } 
     ]
 };
-
-function priceRow(qty, netPrice) {
-    return qty * netPrice;
-}
-
-//global var
-const TAX_RATE = 0.07;
-//formatting functions
-function ccyFormat(num) {
-        return `${num.toFixed(2)}`;
-}
-
-
-
-function subtotal(items) {
-    return items.map(({ netPrice }) => netPrice).reduce((sum, i) => sum + i, 0);
-}
-//result functions calc
-const invoiceSubtotal = subtotal(rows);
-const invoiceTaxes = TAX_RATE * invoiceSubtotal;
-const invoiceTotal = invoiceTaxes + invoiceSubtotal;
 
 export default function SpanningSalesTable() {
     const {
@@ -89,7 +50,6 @@ export default function SpanningSalesTable() {
         setValue,
         watch
     } = useForm({ defaultValues });
-
 
     const onSubmit = (data) => console.log("data", data);
     console.log("dataLog", onSubmit());
@@ -108,13 +68,16 @@ export default function SpanningSalesTable() {
                             getValues,
                             setValue,
                             errors,
-                            
                         }}
                     />
-                    <TableRow><TableCell colSpan={9} sx={{border: 0, p: 0, pt: 2}}> <hr/> </TableCell></TableRow>
-                    <TableTotalSum defaultValuesItem={TESTdefaultValuesSalesItem} />
+                    <TableRow>
+                        <TableCell colSpan={9} sx={{border: 0, p: 0, pt: 2}} />
+                    </TableRow>
+                    <TableTotalSum>
+                        <SalesTotalSum nameSalesList={`salesTableList`} setValue={setValue} control={control} />
+                    </TableTotalSum>
                 </Table>
-                <input type="submit" />
+                {/* <input type="submit" /> */}
             </TableContainer>
                         
         </form>
--- invoice-app/src/components/invoices/new-invoice/invoice-create/invoice-form/subcomponents/sales-table/spanning-sales-table/SalesTableList.jsx
+++ invoice-app/src/components/invoices/new-invoice/invoice-create/invoice-form/subcomponents/sales-table/spanning-sales-table/SalesTableList.jsx
@@ -1,10 +1,11 @@
 import { TableBody, TableRow, TableCell} from "@mui/material";
 import { useFieldArray, useWatch, Controller} from "react-hook-form";
-import { TextField, Select, MenuItem } from "@mui/material";
+import { TextField, Select, MenuItem, IconButton, Button } from "@mui/material";
+import DeleteIcon from '@mui/icons-material/Delete';
 import { NumberInput } from "react-admin";
-import {SelectItemSalesType, SelectTaxSalesItem } from "./item-inputs/SelectorsCells";
-import { ItemNameTextInput, PriceNumberInput, QuantityNumberInput } from "./item-inputs/InputsCells";
-import { IsolateReRenderCell, IsolateReRenderGrossPrice, IsolateReRenderGrossValue, IsolateReRenderNetValue} from "./item-outputs/IsolateReRenderCells";
+import {SelectItemSalesType, SelectTaxSalesItem } from "./sales-item-cells/SelectorsCells";
+import { ItemNameTextInput, PriceNumberInput, QuantityNumberInput } from "./sales-item-cells/InputsCells";
+import { IsolateReRenderCell, IsolateReRenderGrossPrice, IsolateReRenderGrossValue, IsolateReRenderNetValue, ItemRowOutputCells} from "./sales-item-cells/IsolateReRenderCells";
 
 //formatting functions
 // function ccyFormat(num) {
@@ -33,27 +34,31 @@ export default function SalesTableList({ control, register, setValue, getValues,
                 return(
                     <TableRow key={item.id}>
                         <TableCell align="center" sx={{ maxWidth: 15,  pr:0  }} >{index+1}</TableCell>
-                        <ItemNameTextInput name={`salesTableList.${index}.itemName`} control={control} />
-                        <SelectItemSalesType name={`salesTableList.${index}.type`} control={control} />
-                        <QuantityNumberInput name={`salesTableList.${index}.qty`} control={control} defaultValue={item.qty}/>
-                        <PriceNumberInput name={`salesTableList.${index}.netPrice`} control={control} />
-                        <SelectTaxSalesItem name={`salesTableList.${index}.taxValue`} control={control} />
-                        {/* <TableCell align="right"> {"test"}      */}
-                        {/* {`${(item.grossPrice).toFixed(2)}`} */}
-                          {/* </TableCell> */}
-                        <IsolateReRenderGrossPrice control={control} nameItemRow={`salesTableList.${index}`} />
-                        <IsolateReRenderNetValue control={control} nameItemRow={`salesTableList.${index}`} />
-                        <IsolateReRenderGrossValue control={control} nameItemRow={`salesTableList.${index}`} />
+                        <ItemNameTextInput name={`salesTableList.${index}.itemName`}    control={control} />
+                        <SelectItemSalesType name={`salesTableList.${index}.type`}      control={control} />
+                        <QuantityNumberInput name={`salesTableList.${index}.qty`}       control={control} defaultValue={item.qty}/>
+                        <PriceNumberInput name={`salesTableList.${index}.netPrice`}     control={control} />
+                        <SelectTaxSalesItem name={`salesTableList.${index}.taxValue`}   control={control} />
+                        <ItemRowOutputCells nameSalesItem={`salesTableList.${index}`}     control={control} />
                         <TableCell align="right">
-                            <button type="button" onClick={() => remove(index)}> X </button>
+                            {/* <Button size="small" color="error" onClick={() => remove(index)} > */}
+                                <IconButton  color="error"   aria-label="delete" size="small"
+                                    onClick={() => remove(index)} 
+                                >
+                                    <DeleteIcon fontSize="small" />
+                                </IconButton>
+                            {/* </Button> */}
                         </TableCell>
                     </TableRow>
                 )}
             )}
         </TableBody>
         <TableRow><TableCell colSpan={9} sx={{border: 0, p: 0, pt: 2}}> 
-            <div><input type="button" value="+ADD" onClick={() => append(defaultValuesSalesItem)}    />
-            <hr/>
+            <div>
+            <Button onClick={() => append(defaultValuesSalesItem)} variant="contained" size="small" >
+                ADD
+            </Button>
+                {/* // <input type="button" value="+ADD" onClick={() => append(defaultValuesSalesItem)}    /> */}
             <span className="counter">Render Count: {renderCount}</span>
             </div>
         </TableCell></TableRow>
